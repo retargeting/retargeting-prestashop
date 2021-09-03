71a3f6215ed3d25d0e0196a6ee7009dd3f166340
Alex B <alexandru.buzica@retargeting.biz>
2021-09-03 17:24:35 +0300

    someSmallFix

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '74d56df76ce16e59ea612224cc62a8271f2b7f2f',
+    'name' => 'retargeting/ra_tracker',
+  ),
+  'versions' => 
+  array (
+    'retargeting/ra_tracker' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '74d56df76ce16e59ea612224cc62a8271f2b7f2f',
+    ),
+    'retargeting/retargeting-sdk' => 
+    array (
+      'pretty_version' => 'dev-development',
+      'version' => 'dev-development',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c3cf5c5d17bc4357ad21e4ca0d0d38aa0366125b',
+    ),
+  ),
+);
